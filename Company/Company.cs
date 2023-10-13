186d32848374948601b88119d37acf908cbf30ef
ll novbe <ll novbe@DESKTOP-AKQ170U>
2023-10-13 19:12:55 +0400

    Add project files.

---
+++ Company/Company.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Company
+{
+    internal class Company
+    {
+        public User[] users = new User[10];
+        public string Name { get; set; }
+
+
+        public Company(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void Register()
+        {
+
+            Console.WriteLine("Enter the name : ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter the surname : ");
+            string Surname = Console.ReadLine();
+            User user = new User();
+
+            while (true)
+            {
+                user.Name = Name;
+                user.Surname = Surname;
+                Console.WriteLine("Enter the password : ");
+                string PassWord = Console.ReadLine();
+
+
+                if(PassWord.Length >= 5)
+                {
+
+                    string UserEmail = $"{user.Name}.{user.Surname}@gmail.com";
+                    string UserName = $"{user.Name}.{user.Surname}";
+                    Console.WriteLine($"{UserEmail}\n{UserName}");
+                    user.Password = PassWord;
+                    users[0] = user; //Array.resize nan arraya elave eliyeceksen
+                    break;
+                }
+
+            }
+        }
+
+        public void Login(string ) //string Username istiyecek
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Company/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company("Asus");
+            company.Register();
+
+        }
+    }
+}
